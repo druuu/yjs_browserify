8cb519a1ce9738ca3a7e355414190ded2a4ec924
druuu <druuu.mail@gmail.com>
2018-06-14 05:44:31 +0000

    wip, yjs13 support

--- src/app.js
+++ src/app.js
@@ -1,20 +1,8 @@
-require('regenerator').runtime();
-const Y = require('yjs');
-require('y-memory')(Y);
-require('y-webrtc3')(Y);
-require('y-array')(Y);
-require('y-map')(Y);
-require('y-text')(Y);
-require('y-xml')(Y);
-
-Y({
-  db: {
-    name: 'memory'
-  },
+let y = new Y('htmleditor', {
   connector: {
     name: 'webrtc',
     //name: 'websockets-client',
-    room: 'room',
+    room: 'dinesh',
     url: 'http://finwin.io:1256'
   },
   share: {
@@ -39,27 +27,58 @@ Y({
     xml9: 'Xml',
     xml10: 'Xml'
   }
-}).then(function (y) {
-    console.log('############');
-    window.yXml = y;
-    y.share.codemirror.bind(window.shared_elements['codemirror']);
-    y.share.codemirror2.bind(window.shared_elements['codemirror2']);
-    y.share.codemirror3.bind(window.shared_elements['codemirror3']);
-    y.share.codemirror4.bind(window.shared_elements['codemirror4']);
-    y.share.codemirror5.bind(window.shared_elements['codemirror5']);
-    y.share.codemirror6.bind(window.shared_elements['codemirror6']);
-    y.share.codemirror7.bind(window.shared_elements['codemirror7']);
-    y.share.codemirror8.bind(window.shared_elements['codemirror8']);
-    y.share.codemirror9.bind(window.shared_elements['codemirror9']);
-    y.share.codemirror10.bind(window.shared_elements['codemirror10']);
-    y.share.xml._bindToDom(window.shared_elements['xml']);
-    y.share.xml2._bindToDom(window.shared_elements['xml2']);
-    y.share.xml3._bindToDom(window.shared_elements['xml3']);
-    y.share.xml4._bindToDom(window.shared_elements['xml4']);
-    y.share.xml5._bindToDom(window.shared_elements['xml5']);
-    y.share.xml6._bindToDom(window.shared_elements['xml6']);
-    y.share.xml7._bindToDom(window.shared_elements['xml7']);
-    y.share.xml8._bindToDom(window.shared_elements['xml8']);
-    y.share.xml9._bindToDom(window.shared_elements['xml9']);
-    y.share.xml10._bindToDom(window.shared_elements['xml10']);
-})
+});
+
+window.y = y;
+
+var xml = y.define('xml', Y.XmlFragment);
+new Y.DomBinding(xml, window.shared_elements['xml']);
+
+var xml2 = y.define('xml', Y.XmlFragment);
+new Y.DomBinding(xml2, window.shared_elements['xml2']);
+
+
+var codemirror = y.define('text');
+new Y.DomBinding(codemirror, window.shared_elements['codemirror']);
+
+var codemirror2 = y.define('text');
+new Y.DomBinding(codemirror2, window.shared_elements['codemirror2']);
+//
+console.log('############');
+//window.yXml = y;
+//y.share.codemirror.bind(window.shared_elements['codemirror']);
+//y.share.codemirror2.bind(window.shared_elements['codemirror2']);
+//y.share.codemirror3.bind(window.shared_elements['codemirror3']);
+//y.share.codemirror4.bind(window.shared_elements['codemirror4']);
+//y.share.codemirror5.bind(window.shared_elements['codemirror5']);
+//y.share.codemirror6.bind(window.shared_elements['codemirror6']);
+//y.share.codemirror7.bind(window.shared_elements['codemirror7']);
+//y.share.codemirror8.bind(window.shared_elements['codemirror8']);
+//y.share.codemirror9.bind(window.shared_elements['codemirror9']);
+//y.share.codemirror10.bind(window.shared_elements['codemirror10']);
+//y.share.xml._bindToDom(window.shared_elements['xml']);
+//y.share.xml2._bindToDom(window.shared_elements['xml2']);
+//y.share.xml3._bindToDom(window.shared_elements['xml3']);
+//y.share.xml4._bindToDom(window.shared_elements['xml4']);
+//y.share.xml5._bindToDom(window.shared_elements['xml5']);
+//y.share.xml6._bindToDom(window.shared_elements['xml6']);
+//y.share.xml7._bindToDom(window.shared_elements['xml7']);
+//y.share.xml8._bindToDom(window.shared_elements['xml8']);
+//y.share.xml9._bindToDom(window.shared_elements['xml9']);
+//y.share.xml10._bindToDom(window.shared_elements['xml10']);
+
+
+//window.undoManager = new Y.utils.UndoManager(window.yXmlType, {
+//  captureTimeout: 500
+//})
+//
+//document.onkeydown = function interceptUndoRedo (e) {
+//  if (e.keyCode === 90 && (e.metaKey || e.ctrlKey)) {
+//    if (!e.shiftKey) {
+//      window.undoManager.undo()
+//    } else {
+//      window.undoManager.redo()
+//    }
+//    e.preventDefault()
+//  }
+//}
